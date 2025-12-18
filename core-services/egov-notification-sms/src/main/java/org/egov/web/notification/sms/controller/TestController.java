d3bb15f08a2826a528e3164d0f226c59fc88ea97
nehaniua <neha@niua.org>
2025-12-18 18:01:14 +0530

    Integrated sms service for assam

--- core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/controller/TestController.java
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/controller/TestController.java
@@ -21,11 +21,12 @@ public class TestController {
     public ResponseEntity<?> test(@RequestParam(value="number", required = true) String number,
                                   @RequestParam(value="msg", required = true) String msg,
                                   @RequestParam(value="category", required = true) Category category,
-                                  @RequestParam(value="expirytime", required = true) Long expirytime){
+                                  @RequestParam(value="expirytime", required = true) Long expirytime,
+                                  @RequestParam(value = "templateid", required = false) String templateId){
 
         //Sms sms = new Sms(number, sms, Category.OTP, expirytime);
 
-        Sms sms = new Sms(number, msg, category, expirytime, "123");
+        Sms sms = new Sms(number, msg, category, expirytime, templateId);
 
         smsService.sendSMS(sms);
 
