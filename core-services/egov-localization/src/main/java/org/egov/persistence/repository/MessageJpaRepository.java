1d6959bc21ef5c208299446116e7982af33d3ce4
dbarman123 <dbarman@deloitte.com>
2025-12-19 23:55:21 +0530

    Update MessageJpaRepository.java

--- core-services/egov-localization/src/main/java/org/egov/persistence/repository/MessageJpaRepository.java
+++ core-services/egov-localization/src/main/java/org/egov/persistence/repository/MessageJpaRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface MessageJpaRepository extends JpaRepository<Message, String> {
+public interface MessageJpaRepository extends JpaRepository<Message, Long> {
 	@Query("select m from Message m where m.tenantId = :tenantId and m.locale = :locale")
 	List<Message> find(@Param("tenantId") String tenantId, @Param("locale") String locale);
 
